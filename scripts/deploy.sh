53368c86d83e1f2e48ce9d39e1ee1d86b8030d47
Adam Kewley <contact@adamkewley.com>
2025-11-05 09:15:19 +0100

    Add 0.6.2 post, rename upload.sh -> deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rsync -avz public/ www.opensimcreator.com:/var/www/opensimcreator.com/
+
--- scripts/upload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -avz --delete public/ www.opensimcreator.com:/var/www/opensimcreator.com/
-
